85bc1076296fe84cd293ca01d57a6f67bd6166a8
siropaca <ryhshr3@gmail.com>
2022-04-13 23:48:44 +0900

    modify core.js

--- practice/core.js
+++ practice/core.js
@@ -1,77 +1,88 @@
-var canvas, ctx;
-var screenWidth, screenHeight;
-var curYubiX, curYubiY, yubiTouched;
+let canvas, ctx;
+let screenWidth, screenHeight;
+let curYubiX, curYubiY, yubiTouched;
 
-onload = function(){
-	canvas = document.getElementById('canvas');
-	if ( ! canvas || ! canvas.getContext ) {
-		return false;
-	}
-	ctx = canvas.getContext('2d');
-	screenWidth = canvas.width;
-	screenHeight = canvas.height;
-	
-	curYubiX = screenWidth/2;
-	curYubiY = screenHeight/2;
-	
-	//スマホ用
-	canvas.addEventListener("touchstart",function(evt) {
-		evt.preventDefault();
-		var rect = canvas.getBoundingClientRect();
-		var bai = screenWidth/rect.width;
-		yubiTouched = true;
-		curYubiX = (evt.changedTouches[0].pageX - (rect.left + window.pageXOffset))*bai;
-		curYubiY = (evt.changedTouches[0].pageY - (rect.top + window.pageYOffset))*bai;
-		touchStart();
-	}, false);
-	
-	canvas.addEventListener("touchmove", function(evt) {
-		evt.preventDefault();
-		var rect = canvas.getBoundingClientRect();
-		var bai = screenWidth/rect.width;
-		curYubiX = (evt.changedTouches[0].pageX - (rect.left + window.pageXOffset))*bai;
-		curYubiY = (evt.changedTouches[0].pageY - (rect.top + window.pageYOffset))*bai;
-		touchMove();
-	}, false);
-	
-	canvas.addEventListener("touchend",function(evt) {
-		evt.preventDefault();
-		var rect = canvas.getBoundingClientRect();
-		var bai = screenWidth/rect.width;
-		yubiTouched = false;
-		curYubiX = (evt.changedTouches[0].pageX - (rect.left + window.pageXOffset))*bai;
-		curYubiY = (evt.changedTouches[0].pageY - (rect.top + window.pageYOffset))*bai;
-		touchEnd();
-	}, false);
-	
-	//PC用
-	canvas.addEventListener("mousedown", function(evt){
-		var rect = canvas.getBoundingClientRect();
-		yubiTouched = true;
-		var bai = screenWidth/rect.width;
-		curYubiX = (evt.clientX - rect.left)*bai;
-		curYubiY = (evt.clientY - rect.top)*bai;
-		touchStart();
-	}, false);
-	
-	canvas.addEventListener("mousemove", function(evt){
-		var rect = canvas.getBoundingClientRect();
-		var bai = screenWidth/rect.width;
-		curYubiX = (evt.clientX - rect.left)*bai;
-		curYubiY = (evt.clientY - rect.top)*bai;
-		touchMove();
-	}, false);
-	
-	canvas.addEventListener("mouseup", function(evt){
-		var rect = canvas.getBoundingClientRect();
-		yubiTouched = false
-		var bai = screenWidth/rect.width;
-		curYubiX = (evt.clientX - rect.left)*bai;
-		curYubiY = (evt.clientY - rect.top)*bai;
-		touchEnd();
-	}, false);
+onload = function() {
+  canvas = document.getElementById('canvas');
 
-	setup();
-	
-	var timerId = setInterval(loop, 33);
-}
+  if (!canvas || !canvas.getContext) {
+    return false;
+  }
+
+  ctx = canvas.getContext('2d');
+  screenWidth = canvas.width;
+  screenHeight = canvas.height;
+
+  curYubiX = screenWidth / 2;
+  curYubiY = screenHeight / 2;
+
+  //スマホ用
+  canvas.addEventListener('touchstart', (evt) => {
+    evt.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+    const bai = screenWidth / rect.width;
+    yubiTouched = true;
+    curYubiX = (evt.changedTouches[0].pageX - (rect.left + window.pageXOffset)) * bai;
+    curYubiY = (evt.changedTouches[0].pageY - (rect.top + window.pageYOffset)) * bai;
+
+    touchStart();
+  }, false);
+
+  canvas.addEventListener('touchmove', (evt) => {
+    evt.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+    const bai = screenWidth / rect.width;
+    curYubiX = (evt.changedTouches[0].pageX - (rect.left + window.pageXOffset)) * bai;
+    curYubiY = (evt.changedTouches[0].pageY - (rect.top + window.pageYOffset)) * bai;
+
+    touchMove();
+  }, false);
+
+  canvas.addEventListener('touchend', (evt) => {
+    evt.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+    const bai = screenWidth / rect.width;
+    yubiTouched = false;
+    curYubiX = (evt.changedTouches[0].pageX - (rect.left + window.pageXOffset)) * bai;
+    curYubiY = (evt.changedTouches[0].pageY - (rect.top + window.pageYOffset)) * bai;
+
+    touchEnd();
+  }, false);
+
+  // PC用
+  canvas.addEventListener('mousedown', (evt) => {
+    const rect = canvas.getBoundingClientRect();
+    yubiTouched = true;
+    const bai = screenWidth / rect.width;
+    curYubiX = (evt.clientX - rect.left) * bai;
+    curYubiY = (evt.clientY - rect.top) * bai;
+
+    touchStart();
+  }, false);
+
+  canvas.addEventListener('mousemove', (evt) => {
+    const rect = canvas.getBoundingClientRect();
+    const bai = screenWidth / rect.width;
+    curYubiX = (evt.clientX - rect.left) * bai;
+    curYubiY = (evt.clientY - rect.top) * bai;
+
+    touchMove();
+  }, false);
+
+  canvas.addEventListener('mouseup', (evt) => {
+    const rect = canvas.getBoundingClientRect();
+    yubiTouched = false;
+    const bai = screenWidth / rect.width;
+    curYubiX = (evt.clientX - rect.left) * bai;
+    curYubiY = (evt.clientY - rect.top) * bai;
+
+    touchEnd();
+  }, false);
+
+  setup();
+
+  const timerId = setInterval(loop, 33);
+};
