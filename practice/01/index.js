0ee4ada1f8f237cd9634c77af6e1a6e4e2a99545
siropaca <ryhshr3@gmail.com>
2022-04-13 23:42:40 +0900

    add practice

---
+++ practice/01/index.js
@@ -0,0 +1,19 @@
+function setup() {	// 最初に実行される
+
+}
+
+function loop() {	// 常時実行される
+
+}
+
+function touchStart() {	// タッチ(マウスダウン)されたら
+
+}
+
+function touchMove() {	// 指が動いたら(マウスが動いたら)
+
+}
+
+function touchEnd() {	// 指が離されたら(マウスアップ)
+
+}
