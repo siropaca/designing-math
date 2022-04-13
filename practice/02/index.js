dbb5325b064c3ee186868524ce72c7c76a0e5b37
siropaca <ryhshr3@gmail.com>
2022-04-14 00:12:50 +0900

    add practice02

--- practice/02/index.js
+++ practice/02/index.js
@@ -1,5 +1,17 @@
 function setup() {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(screenWidth, screenHeight);
+  ctx.lineTo(screenWidth / 3, screenHeight);
+  ctx.lineTo(0, 0);
+  ctx.stroke();
 
+  ctx.beginPath();
+  ctx.moveTo(screenWidth * 0.5, screenHeight * 0.1);
+  ctx.lineTo(screenWidth, 0);
+  ctx.lineTo(screenWidth * 0.8, screenHeight * 0.6);
+  ctx.lineTo(screenWidth * 0.4, screenHeight * 0.2);
+  ctx.fill();
 }
 
 function loop() {
